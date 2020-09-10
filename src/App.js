5dd9d853dbe1f764b32a3eacb26bb0bd03de01dc
austinwilson1224 <austin1224@gmail.com>
2020-09-09 17:52:45 -0700

    added api stuff

--- src/App.js
+++ src/App.js
@@ -12,12 +12,27 @@ import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 
 
+import { fetchData } from './api';
+
 
 export default class App extends Component {
+
+
+  state = {
+    data: {},
+  }
+
+
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData});
+  }
   render() {
+    const { data } = this.state;
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={data}/>
         <CountryPicker />
         <Chart />
       </div>
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,44 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import styles from './Cards.module.css';
 
-const Cards = () => {
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+    // console.log(props);
+    console.log(confirmed);
+    if (!confirmed) {
+        return '...loading';
+    }
     return (
-        <h1>Cards</h1>
+        <div className={"styles.container"}>
+            <Grid container spacing={3} justify="center">
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                        <Typography variant="h5">{confirmed.value}</Typography>
+                        <Typography color="textSecondary">REAL DATE</Typography>
+                        <Typography variant="body2">Number of active cases of COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                        <Typography variant="h5">{recovered.value}</Typography>
+                        <Typography color="textSecondary">REAL DATE</Typography>
+                        <Typography variant="body2">Number of recovered cases from COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                        <Typography variant="h5">{deaths.value}</Typography>
+                        <Typography color="textSecondary">REAL DATE</Typography>
+                        <Typography variant="body2">Number of deaths caused by COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+
+        </div>
+
     )
 }
 
